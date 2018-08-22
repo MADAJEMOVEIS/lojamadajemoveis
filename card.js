6e567518c1d9fb7b90c7335a78bdd5a44a319e96
MADAJEMOVEIS <42530194+MADAJEMOVEIS@users.noreply.github.com>
2018-08-22 02:18:06 -0300

    Update card.js

--- card.js
+++ card.js
@@ -704,7 +704,7 @@ optionsString = optionsString.substring(0,180) + "...";
 
 
 	me.currencyStringForPaypalCheckout = function( value ){
-		/*if( me.currencySymbol() == "&#36;" ){
+		/*if( me.currencySymbol() == "R$" ){
 			return "$" + parseFloat( value ).toFixed(2);
 		} else {
 			return "" + parseFloat(value ).toFixed(2);
